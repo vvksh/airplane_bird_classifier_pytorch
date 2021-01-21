df524973edcfe511f2b10b386ca33c94dc469f67
Vivek Sah <vivek.sah32+github@gmail.com>
2021-01-20 22:42:36 -0800

    Add val_dataloader to check accuracy

--- classifier.py
+++ classifier.py
@@ -12,9 +12,24 @@ class AirplaneBirdClassifier:
         )
         self.loss_fn  = nn.NLLLoss()
 
+    def _get_accuracy(self, val_dataloader):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for imgs, labels in val_dataloader:
+                batch_size = imgs.shape[0]
+                outputs = self.model(imgs.view(batch_size, -1))
+                _, predicted = torch.max(outputs, dim=1)
+                total += labels.shape[0]
+                correct += int((predicted == labels).sum())
+
+        return correct / total
+
+
     def training_loop(self,
                       n_epochs: int,
                       train_dataloader: DataLoader,
+                      val_dataloader: DataLoader,
                       optimizer_class: torch.optim.Optimizer.__class__,
                       learning_rate: float):
         optimizer = optimizer_class(self.model.parameters(), lr=learning_rate)
@@ -28,7 +43,8 @@ class AirplaneBirdClassifier:
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
-            print("Epoch: %d, Loss: %f" % (epoch, float(loss)))
+
+            print(f"Epoch: {epoch}, Loss: {float(loss)}, Accuracy {self._get_accuracy(val_dataloader)}")
 
 
 
--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import torch
 from torch.utils.data.dataloader import DataLoader
 
 def run_classifier():
-    cifar2_train_dataset, _ = data_utils.get_cifar2_train_test_dataset()
+    cifar2_train_dataset, cifar2_val_dataset = data_utils.get_cifar2_train_test_dataset()
     cifar2_train_dataloader = DataLoader(dataset=cifar2_train_dataset,
                                          batch_size=64,
                                          shuffle=True)
+    cifar2_val_loader = torch.utils.data.DataLoader(dataset=cifar2_val_dataset,
+                                                    batch_size=64,
+                                                    shuffle=False)
+
     classifier = AirplaneBirdClassifier()
     classifier.training_loop(n_epochs=100,
                              train_dataloader=cifar2_train_dataloader,
+                             val_dataloader=cifar2_val_loader,
                              optimizer_class=torch.optim.SGD,
                              learning_rate=1e-2)
 
